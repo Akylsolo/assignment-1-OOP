bb1198a52fc928b9c2061ee50905ef2863cb379e
Admin <muratbekakyl@gmail.com>
2025-12-13 13:50:39 +0500

    HHJHJ

---
+++ src/Main2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+void main() {
+    Scanner as = new Scanner(System.in);
+    int a = as.nextInt();
+    int d = a;
+
+
+    for(int i = 1;i<=10;i++){
+
+
+
+            a*=i;
+
+
+            System.out.println(a);
+            a =d;
+
+
+
+
+
+
+
+    }
+}
